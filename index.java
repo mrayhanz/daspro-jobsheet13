4af71feb5f8b244ffdd5a60a26fccb000a2c7d90
mrayhanz <rayymekdi@gmail.com>
2024-12-02 14:26:27 +0700

    first commit

--- index.java
+++ index.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class index {
+    Scanner sc = new Scanner(System.in);
 
     static void input(){
-        
+
     }
     public static void main(String[] args) {
         
