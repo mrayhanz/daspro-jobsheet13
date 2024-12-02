f5ec543403d9e306276992a56d05ee3323598c7d
mrayhanz <rayymekdi@gmail.com>
2024-12-02 18:04:43 +0700

    input dan main

--- index.java
+++ index.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class index {
-    static Scanner sc = new Scanner(System.in);
     static String dataPrestasi[][] = new String[100][5];
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int pilihan;
         do {
             System.out.println("=== PENCATATAN PRESTASI MAHASISWA ===");
@@ -11,11 +11,12 @@ public class index {
             System.out.println("2. Tampilkan Semua Prestasi");
             System.out.println("3. Analisis Prestasi Berdasarkan Jenis");
             System.out.println("4. Keluar");
+            System.out.print("Pilih Menu: ");
             pilihan = sc.nextInt();
             if (pilihan == 1) {
                 tambahPrestasi();
             }else if (pilihan == 2) {
-                System.out.println("=== DAFTAR SEMUA PRESTASI ===");
+
             }else if (pilihan == 3) {
                 System.out.println("=== ANALISIS PRESTASI ===");
             }else if (pilihan == 4) {
@@ -27,31 +28,34 @@ public class index {
         } while (pilihan!= 4);   
     }
     static void tambahPrestasi(){
-        System.out.println("Masukkan Nama Mahasiswa: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Nama Mahasiswa: ");
         String nama = sc.nextLine();
-        System.out.println("Masukkan NIM Mahasiswa: ");
+        System.out.print("Masukkan NIM Mahasiswa: ");
         String nim = sc.nextLine();
-        System.out.println("Masukkan Jenis Prestasi: ");
+        System.out.print("Masukkan Jenis Prestasi: ");
         String jenis = sc.nextLine();
-        System.out.println("Masukkan Tingkat Prestasi (Lokal/Nasional/Internasional): ");
+        System.out.print("Masukkan Tingkat Prestasi (Lokal/Nasional/Internasional): ");
         String tingkat = sc.nextLine();
-        System.out.println("Masukkan Tahun Prestasi: ");
-        int tahun = sc.nextInt();
-        if (tahun < 2010 || tahun > 2024) {
-            System.out.println("Tahun tidak valid coba lagi.");
-            return;
-        }
-        for (int i = 1; i < dataPrestasi.length; i++) {
-            if (dataPrestasi[i][0] == null) {
-                dataPrestasi[i][0] = nama;
-                dataPrestasi[i][1] = nim;
-                dataPrestasi[i][2] = jenis;
-                dataPrestasi[i][3] = tingkat;
-                dataPrestasi[i][4] = Integer.toString(tahun);
-                System.out.println("Data Prestasi Berhasil ditambahkan.");
-                break;
+        do {
+            System.out.print("Masukkan Tahun Prestasi: ");
+            int tahun = sc.nextInt();
+            if (tahun < 2010 || tahun > 2024) {
+                System.out.println("Tahun tidak valid coba lagi.");
+            }else {
+                for (int i = 0; i < dataPrestasi.length; i++) {
+                    if (dataPrestasi[i][0] == null) {
+                        dataPrestasi[i][0] = nama;
+                        dataPrestasi[i][1] = nim;
+                        dataPrestasi[i][2] = jenis;
+                        dataPrestasi[i][3] = tingkat;
+                        dataPrestasi[i][4] = Integer.toString(tahun);
+                        System.out.println("Data Prestasi Berhasil ditambahkan.");
+                        break;
+                    }
+                }
+                break;            
             }
-        }
-        System.out.println("Data Prestasi Berhasil ditambahkan.");
+        } while (true);
     }
 }
