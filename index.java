7758eeb85c511010661e74a1e1c4705aae3a884f
Ahmad-Zainudin-Fanani <azfann.natinons@gmail.com>
2024-12-02 17:50:59 +0700

    menampilkan semua prestasi

--- index.java
+++ index.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class index {
     static Scanner sc = new Scanner(System.in);
     static String dataPrestasi[][] = new String[100][5];
+    static int jumlahPrestasi = 0;
     public static void main(String[] args) {
         int pilihan;
         do {
@@ -31,15 +32,46 @@ public class index {
         String nama = sc.nextLine();
         System.out.println("Masukkan NIM Mahasiswa: ");
         String nim = sc.nextLine();
-        System.out.println("Masukkan Jenis Prestasi (Lokal/Nasional/Internasional): ");
+        System.out.println("Masukkan Jenis Prestasi: ");
         String jenis = sc.nextLine();
+        System.out.println("Masukkan Tingkat Prestasi (Lokal/Nasional/Internasional): ");
+        String tingkat = sc.nextLine();
         System.out.println("Masukkan Tahun Prestasi: ");
         int tahun = sc.nextInt();
         if (tahun < 2010 || tahun > 2024) {
             System.out.println("Tahun tidak valid coba lagi.");
             return;
         }
-
-        System.out.println("Data Prestasi Berhasil ditambahkan.");
+        for (int i = 1; i < dataPrestasi.length; i++) {
+            if (dataPrestasi[i][0] == null) {
+                dataPrestasi[i][0] = nama;
+                dataPrestasi[i][1] = nim;
+                dataPrestasi[i][2] = jenis;
+                dataPrestasi[i][3] = tingkat;
+                dataPrestasi[i][4] = Integer.toString(tahun);
+                System.out.println("Data Prestasi Berhasil ditambahkan.");
+                break;
+            }
+        }
     }
+        // Fungsi untuk menampilkan semua prestasi yang telah dimasukkan
+    static void tampilkanPrestasi(){
+        if (jumlahPrestasi == 0) {
+        // Jika tidak ada data prestasi
+        System.out.println("Belum ada data prestasi.");
+        } else {
+        // Jika ada data prestasi, lakukan iterasi
+        for (int i = 0; i < jumlahPrestasi; i++) {
+            // Menampilkan setiap data prestasi
+            System.out.printf("Nama: %s | NIM: %s | Jenis: %s | Tingkat: %s | Tahun: %s%n",
+                              dataPrestasi[i][0],  // Nama
+                              dataPrestasi[i][1],  // NIM
+                              dataPrestasi[i][2],  // Jenis Prestasi
+                              dataPrestasi[i][3],  // Tingkat Prestasi
+                              dataPrestasi[i][4]); // Tahun
+        }
+    }
+}
+
+    
 }
